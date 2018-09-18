fec01dbf28f4757c3bc35578aeae55c365dbda66
Shilpa.Shripad <hegde.shilpa.s@gmail.com>
2018-09-19 00:51:16 +0530

    updated script

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makecacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  ## Cache of inverse of matrix with Lexical scoping
+  ## functions do
+  
+  ## This function creates the object inverse of the matrix and appears to be stored in the cache
+  
+  makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) m <<- solve
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+    
+  }
+  
+  
+  ## The function returns a matrix which is inverse of x, if there is already stored cache,
+  #The cached value is returned
+  
+  cacheSolve <- function(x, ...) {
+    ## Return a matrix that is the inverse of 'x'
+    if(!is.null(m)) {
+      message("getting cached data")
+      return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
+  }
+  
 }
